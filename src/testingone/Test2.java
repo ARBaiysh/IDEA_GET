9e60bf9c0ce3b73cfe900e2736717dc63d16962e
Workstation <o0052ib@gmail.com>
2021-07-17 03:05:08 +0600

    6

--- src/testingone/Test2.java
+++ src/testingone/Test2.java
@@ -3,5 +3,7 @@ package testingone;
 public class Test2 {
     public static void main(String[] args) {
         System.out.println(Test2.class.getName());
+        System.out.println(Test2.class.getName());
+        System.out.println(Test2.class.getName());
     }
 }
