c17388716d3b4b59e7807a0fd8e2e8478b67a023
Workstation <o0052ib@gmail.com>
2021-07-17 18:09:43 +0600

    add User2

--- java/test/Test1.java
+++ java/test/Test1.java
@@ -7,5 +7,7 @@ public class Test1 {
     public static void main(String[] args) {
         User user = new UserBuilder().withPassword("dnweapons7s").withName("Baiysh").build();
         System.out.println(user);
+        User user2 = new UserBuilder().withPassword("dnweapons7s333333").withName("Baiysh333333333").build();
+        System.out.println(user2);
     }
 }
